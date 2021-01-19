a63aa53522ae69f891c23c8ca1972bc0fb088960
张祥 <zhangxiang@hua-cloud.com.cn>
2021-01-19 15:51:26 +0800

    正则修改

--- node-generate/utils/regExps.js
+++ node-generate/utils/regExps.js
@@ -1,8 +1,13 @@
 /**
  * 匹配空白字符串和引号
  */
-export const spaceQuote = /\s|\'/g
+const spaceQuote = /\s|\'/g
 /**
  * 匹配空白字符串和逗号
  */
-export const spaceComma = /\s|,/g
+const spaceComma = /\s|,/g
+
+module.exports = {
+  spaceQuote,
+  spaceComma,
+}
